fc314051064f213d754adff92be57d038796a1f7
JustinChernokal <jcherns@udel.edu>
2020-02-27 17:05:25 -0500

    Updates

---
+++ dev/precache-manifest.2b02a22ca410869a392729d763c7cf76.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-JustinChernokal/devassets/citations.md"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-JustinChernokal/devassets/images/background.png"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-JustinChernokal/devassets/images/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-JustinChernokal/devassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-JustinChernokal/devassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-JustinChernokal/devassets/images/ship3.png"
+  },
+  {
+    "revision": "c17322d91dae9dc93e3b",
+    "url": "/coding-2-try-phaser-JustinChernokal/devgen.main.30ca9917e3495a9aa3fd.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-JustinChernokal/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "252ae2b651b7f9a91839e451b6fe4e61",
+    "url": "/coding-2-try-phaser-JustinChernokal/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-JustinChernokal/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-JustinChernokal/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/coding-2-try-phaser-justinchernokal/devprecache-manifest.0b8df9025ad7454464307e811cad54c3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-JustinChernokal/devprecache-manifest.2b02a22ca410869a392729d763c7cf76.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
