7035684af3204622bdb3970f12c4dba4d03d6bcf
JustinChernokal <jcherns@udel.edu>
2020-02-27 17:50:03 -0500

    wooo9

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,6 +2,9 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  private ship1: ExampleObject;
+  private ship2: ExampleObject;
+  private ship3: ExampleObject;
 
   constructor() {
     super({ key: 'MainScene' });
@@ -12,9 +15,9 @@ export default class MainScene extends Phaser.Scene {
     this.add.image(this.scale.width/2, this.scale.height/2,"background");
     //image.setOrigin(0,0);
 
-    this.add.image(this.scale.width/2 - 50, this.scale.height/2, "ship");
-    this.add.image(this.scale.width/2, this.scale.height/2, "ship2");
-    this.add.image(this.scale.width/2 + 50, this.scale.height/2, "ship3");
+    this.ship1 = this.add.sprite(this.scale.width/2 - 50, this.scale.height/2, "ship");
+    this.ship2 = this.add.sprite(this.scale.width/2, this.scale.height/2, "ship2");
+    this.ship3 = this.add.sprite(this.scale.width/2 + 50, this.scale.height/2, "ship3");
 
     this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
   }
@@ -24,5 +27,8 @@ export default class MainScene extends Phaser.Scene {
   }
 
   update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
   }
 }
