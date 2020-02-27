3062857a3a7aea561be76330a1119528cee454fb
JustinChernokal <jcherns@udel.edu>
2020-02-27 11:26:45 -0500

    Updates

---
+++ dev/precache-manifest.f01f3b532453c81e06893faac04050e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-justinchernokal/devassets/citations.md"
+  },
+  {
+    "revision": "b7e37f4205aa434ff28e",
+    "url": "/coding-2-try-phaser-justinchernokal/devgen.main.b64069b0fa72c664b289.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-justinchernokal/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "10e127510a3b15bb771fa2e225f1f990",
+    "url": "/coding-2-try-phaser-justinchernokal/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-justinchernokal/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-justinchernokal/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/devprecache-manifest.549ef5a400021d602b1a18ffdc9f4103.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-justinchernokal/devprecache-manifest.f01f3b532453c81e06893faac04050e6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
