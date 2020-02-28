66273a40b1e58b61b1f79f412ad06602cd79b93f
JustinChernokal <jcherns@udel.edu>
2020-02-27 19:00:29 -0500

    woo14

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -6,6 +6,7 @@ export default class MainScene extends Phaser.Scene {
   private ship1: ExampleObject;
   private ship2: ExampleObject;
   private ship3: ExampleObject;
+  private explosion: ExampleObject;
   private background: TileSprite;
   
 
@@ -23,6 +24,41 @@ export default class MainScene extends Phaser.Scene {
     this.ship1 = this.add.sprite(this.scale.width/2 - 50, this.scale.height/2, "ship");
     this.ship2 = this.add.sprite(this.scale.width/2, this.scale.height/2, "ship2");
     this.ship3 = this.add.sprite(this.scale.width/2 + 50, this.scale.height/2, "ship3");
+    this.explosion = this.add.sprite(0, 0, "explosion");
+
+    this.anims.create({
+      key: "ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship", { start: 0, end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship2", { start: 0, end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship3", { start: 0, end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "explode",
+      frames: this.anims.generateFrameNumbers("explosion", { start: 0, end: 4}),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
+    });
+
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+
 
     this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
   }
